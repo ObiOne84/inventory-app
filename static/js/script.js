cdb0d9f2d23cd84e451ff23c766a1bfc2eb225fd
ObiOne84 <stepienszymon@hotmail.com>
2024-05-15 20:52:46 +0000

    Add footer structure

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+$(document).ready(function () {
+    // Update the page date
+    $('#copyright').text(new Date().getFullYear());
+
+    // Show the footer
+    $('.footer-arrow').on('click', function () {
+        $(this).addClass('d-none');
+        if ($(this).hasClass('arrow-up')) {
+            $('.arrow-down').removeClass('d-none');
+            $('#footer').collapse('show');
+        } else {
+            $('.arrow-up').removeClass('d-none');
+            $('#footer').collapse('hide');
+        }
+    });
+});
