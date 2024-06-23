4d0ec7c29e0eacdbfbe181cb5ec7dc75dace1c02
ObiOne84 <stepienszymon@hotmail.com>
2024-06-23 20:05:04 +0000

    Add products to products page

--- product/views.py
+++ product/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 
+from .models import Product, Category, Subcategory
+
 
 def all_products(request):
     """ A view to display all products """
+    products = Product.objects.all()
+
 
     template = 'product/products.html'
     context = {
+        'products': products,
         
     }
 
